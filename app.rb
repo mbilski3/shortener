90c6d4af475856ad52b8788d278dcf44c5e563b5
Mateusz Bilski <mbilski@appriss.com>
2018-11-22 12:28:51 +0100

    Initial commit

---
+++ app.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'securerandom'
+require 'redis'
+
+configure do
+  uri = URI.parse(ENV['REDIS_URL'])
+  @redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+end
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  @rand = SecureRandom.hex(4)
+  @redis.set(@rand, params[:url])
+  erb :result
+end
+
+get '/:rand' do
+  redirect @redis.get(@rand)
+end
